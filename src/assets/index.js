147d4e43ac107136dff5db075b8c4f84011d4877
Tarun <tarunchikkam@gmail.com>
2025-07-14 12:43:41 +0530

    Removed IBM/VIT logo references to fix build

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,17 +6,17 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import gsap from './tech/gsap.png'
+import gsap from './tech/gsap.png';
 import framer from './tech/framer.png';
 import css from "./tech/css.png";
 import figma from "./tech/figma.png";
 import gearXpert from './gearXpert.png';
 import project3 from './project3.webp';
-import project2 from './proj1.webp'
+import project2 from './proj1.webp';
 import mysql from './tech/mysql.png';
 import express from './tech/express.png';
 import aws from './tech/aws.png';
-import mui from './tech/mui.png'
+import mui from './tech/mui.png';
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
@@ -28,7 +28,7 @@ import tailwind from "./tech/tailwind.png";
 import threejs from "./tech/threejs.svg";
 import firstTestimonial from './Tars Ken.webp';
 import secondTestimonial from './image.webp';
-import thirdTestimonial from './third testimonial.webp'
+import thirdTestimonial from './third testimonial.webp';
 
 export {
   logo,
@@ -62,5 +62,4 @@ export {
   redux,
   tailwind,
   threejs,
-
 };
